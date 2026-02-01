773206f0963987e1d5423b27fbe7203904548a75
dftre <3066417822@qq.com>
2026-02-01 18:38:30 +0800

    修复storage的及时读写导致获取上一次值的问题

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,8 +1,9 @@
 const constant = {
-   avatar: 'vuex_avatar',
-   name: 'vuex_name',
-   roles: 'vuex_roles',
-   permissions: 'vuex_permissions'
- }
+  id: 'vuex_id',
+  avatar: 'vuex_avatar',
+  name: 'vuex_name',
+  roles: 'vuex_roles',
+  permissions: 'vuex_permissions'
+}
 
- export default constant
+export default constant
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -4,10 +4,7 @@ import constant from './constant'
 let storageKey = 'storage_data'
 
 // 存储节点变量名
-let storageNodeKeys = [constant.avatar, constant.name, constant.roles, constant.permissions]
-
-// 存储的数据
-let storageData = uni.getStorageSync(storageKey) || {}
+let storageNodeKeys = [constant.avatar, constant.id, constant.name, constant.roles, constant.permissions]
 
 const storage = {
   set: function(key:string, value:any) {
@@ -19,9 +16,11 @@ const storage = {
     }
   },
   get: function(key:string) {
+    let storageData = uni.getStorageSync(storageKey) || {}
     return storageData[key] || ""
   },
   remove: function(key:string) {
+    let storageData = uni.getStorageSync(storageKey) || {}
     delete storageData[key]
     uni.setStorageSync(storageKey, storageData)
   },
